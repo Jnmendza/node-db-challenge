98a63eb6c61e03354300aeb50e1201f5fdd1e80b
Jnmendza <jonatmendoza@gmail.com>
2019-11-08 09:02:08 -0800

    added index server and migration

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const tasksRouter = require('./routers/tasksRouter')
+const projectsRouter = require('./routers/projectsRouter')
+
+const server = express();
+
+server.use(helmet())
+server.use(express.json())
+
+server.use('/api/tasks', tasksRouter)
+server.use('/api/projects', projectsRouter)
+
+server.get('/', (req, res) => {
+    res.send('Server is running')
+})
+
+module.exports = server;
---
+++ data/migrations/20191108083904_bootstrapdb.js
@@ -0,0 +1,59 @@
+
+exports.up = function(knex) {
+  return knex.schema
+  // PROJECTS TABLE ***********************
+    .createTable('projects', tbl => {
+        tbl.increments()
+
+        tbl.string('project_name', 255).notNullable()
+        tbl.string('project_description', 255)
+        tbl.boolean('completed').defaultTo(false).notNullable();
+    })
+// TASKS TABLE *****************************
+    .createTable('tasks', tbl => {
+        tbl.increments();
+
+        tbl.string('task_description', 255).notNullable()
+        tbl.string('task_notes', 255)
+        tbl.boolean('completed').defaultTo(false).notNullable();
+
+        tbl.integer('project_id')
+            .unsigned()
+            .references('id')
+            .inTable('projects')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+    })
+// RESOURCES TABLE ******************
+    .createTable('resources', tbl => {
+        tbl.increments()
+
+        tbl.string('resource_name', 255).notNullable()
+        tbl.string('resource_description', 255)
+    })
+// PROJECT_RESOURCES
+    .createTable('project_resources', tbl => {
+        tbl.integer('project_id')
+            .unsigned()
+            .references('id')
+            .inTable('projects')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+
+        tbl.integer('resource_id')
+            unsigned()
+            references('id')
+            .inTable('resources')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+    })
+
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('projects')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('project_resources')
+};
